d6a57c9c106c471846c60049f58f789bf68b888d
hoon3284 <hoon3284@icloud.com>
2020-10-01 21:09:54 +0900

    LoginActivity

--- app/src/main/java/kr/ac/daejin/restaurantnews/activity/JoinActivity.java
+++ app/src/main/java/kr/ac/daejin/restaurantnews/activity/JoinActivity.java
@@ -1,50 +1,111 @@
 package kr.ac.daejin.restaurantnews.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.proto.TargetGlobal;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 import kr.ac.daejin.restaurantnews.R;
 import kr.ac.daejin.restaurantnews.databinding.ActivityJoinBinding;
 
-/**
- * Fragment Life Style
- * 1. Fragment is added
- * 2. onAttach()                    Fragment가 Activty에 붙을때 호출
- * 3. onCreate()                    Activty에서의 onCreate()와 비슷하나, ui 관련 작업은 할 수 없다.
- * 4. onCreateView()                Layout을 inflater을 하여 View 작업을 하는 곳
- * 5. onActivityCreated()           Activity에서 Fragment를 모두 생성하고난 다음에 호출됨. Activty의 onCreate()에서 setContentView()한 다음과 같다
- * 6. onStart()                     Fragment가 화면에 표시될때 호출, 사용자의 Action과 상호 작용이 불가능함
- * 7. onResume()                    Fragment가 화면에 완전히 그렸으며, 사용자의 Action과 상호 작용이 가능함
- * 8. Fragment is active
- * 9. User navigates backward or fragment is removed/replaced  or Fragment is added to the back stack, then removed/replaced
- * 10. onPause()
- * 11. onStop()                     Fragment가 화면에서 더이상 보여지지 않게됬을때
- * 12. onDestroy()                  View 리소스를 해제할수있도록 호출. backstack을 사용했다면 Fragment를 다시 돌아갈때 onCreateView()가 호출됨
- * 13. onDetached()
- * 14. Fragment is destroyed
- */
-
-/**
- * Activity LifeCycle
- * onCreate()
- * onStart() <-- onRestart()
- * onResume() <-- onPause()
- * onPause() --> onResume()
- * onStop() --> onRestart()
- * onDestroy()
- */
-
 public class JoinActivity extends AppCompatActivity {
-    ActivityJoinBinding binding;
+    private ActivityJoinBinding binding;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth mAuth;
+    private static final String TAG = "현재";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_join);
         binding.setActivity(this);
+        mAuth = FirebaseAuth.getInstance();
 
+        binding.join.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String email = binding.email.getText().toString();
+                final String password = binding.pw.getText().toString();
+                final String passwordCheck = binding.pwCheck.getText().toString();
+                final String name = binding.name.getText().toString();
+                final String phone = binding.phone.getText().toString();
+                final String birth = binding.birth.getText().toString();
 
+                if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    Toast.makeText(getApplicationContext(), "이메일 형식이 아닙니다.", Toast.LENGTH_SHORT).show();
+                    updateUI(null);
+                } else if(!Pattern.matches("^(?=.*\\d)(?=.*[~`!@#$%\\^&*()-])(?=.*[a-zA-Z]).{8,20}$", password)) {
+                    Toast.makeText(getApplicationContext(), "비밀번호 양식을 지켜주세요.", Toast.LENGTH_SHORT).show();
+                    updateUI(null);
+                } else if(!password.equals(passwordCheck)){
+                    Toast.makeText(getApplicationContext(), "비밀번호가 다릅니다.", Toast.LENGTH_SHORT);
+                } else if(!Patterns.PHONE.matcher(phone).matches()) {
+                    Toast.makeText(getApplicationContext(), "올바른 핸드폰 번호가 아닙니다.", Toast.LENGTH_SHORT).show();
+                    updateUI(null);
+                }
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(JoinActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Log.d(TAG, "onComplete: " + email + password);
+                                if (task.isSuccessful()) {
+                                    final FirebaseUser user = mAuth.getCurrentUser();
+                                    Map<String, Object> userData = new HashMap<>();
+                                    userData.put("phone", phone);
+                                    userData.put("name", name);
+
+                                    db.collection("users").document(user.getUid()).set(userData).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            Log.d(TAG, "DocumentSnapshot added with ID: " + user.getUid());
+                                            updateUI(user);
+                                        }
+                                    }).addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Log.w(TAG, "Error adding document", e);
+                                        }
+                                    });
+                                } else {
+//                                    Toast.makeText(getApplicationContext(), "failed.", Toast.LENGTH_SHORT).show();
+                                    updateUI(null);
+                                }
+                            }
+                        });
+            }
+        });
+
+    }
+    private void updateUI(FirebaseUser account) {
+        if(account != null) {
+            Toast.makeText(this, "성공적으로 회원가입 되었습니다. 다시 로그인해주세요.", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(this, LoginActivity.class));
+        } else {
+            Toast.makeText(this, "회원가입에 실패했습니다.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/kr/ac/daejin/restaurantnews/activity/LoginActivity.java
+++ app/src/main/java/kr/ac/daejin/restaurantnews/activity/LoginActivity.java
@@ -1,27 +1,35 @@
 package kr.ac.daejin.restaurantnews.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import kr.ac.daejin.restaurantnews.R;
 import kr.ac.daejin.restaurantnews.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
-    ActivityLoginBinding binding;
+    private ActivityLoginBinding binding;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
         binding.setActivity(this);
-
-        final FirebaseFirestore db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
 
         binding.buttonJoin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,12 +42,32 @@ public class LoginActivity extends AppCompatActivity {
         binding.buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String id = binding.id.getText().toString();
+                String email = binding.email.getText().toString();
                 String password = binding.pw.getText().toString();
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
+
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            Toast.makeText(getApplicationContext(), "로그인에 실패했습니다. ", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
             }
         });
+    }
+
+    private void updateUI(FirebaseUser account) {
+        if(account != null) {
+            Toast.makeText(this, "성공적으로 로그인 되었습니다.", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(this, MainActivity.class));
+        } else {
+            Toast.makeText(this, "회원가입에 실패했습니다.", Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
