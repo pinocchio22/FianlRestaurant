db6dfa6a8de9a7c48621ff5646439704b605c462
pinocchio22 <vviian.2@gamil.com>
2020-10-05 18:27:04 +0900

    11

---
+++ app/src/main/java/kr/ac/daejin/restaurantnews/FragmentCamera.java
@@ -0,0 +1,17 @@
+package kr.ac.daejin.restaurantnews;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import kr.ac.daejin.restaurantnews.R;
+
+public class FragmentCamera extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_camera, container, false);
+    }
+}
---
+++ app/src/main/java/kr/ac/daejin/restaurantnews/FragmentSearch.java
@@ -0,0 +1,18 @@
+package kr.ac.daejin.restaurantnews;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import kr.ac.daejin.restaurantnews.R;
+
+public class FragmentSearch extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+}
